c82ccf9c2c2ce10650ac0ad638f3bdf6207f9851
tecerwyq <1025981817@qq.com>
2024-03-19 14:04:17 +0800

    规范 UIScreenBase 和 UIScreenManager 的代码

--- Assets/UIFramework/Runtime/Scripts/UIScreenBase.cs
+++ Assets/UIFramework/Runtime/Scripts/UIScreenBase.cs
@@ -144,7 +144,7 @@ namespace UIFramework
             {
                 return false;
             }
-          
+
             // 先从集合中移除，此时已经无法从外部获取到这个Popup
             if (uiPopups.TryRemove(script, out _))
             {
@@ -186,12 +186,10 @@ namespace UIFramework
         {
             return uiPopups.ContainsKey(popup);
         }
-    }
 
-    public class ScreenContext
-    {
-        public string key => screen.screenName;
-
-        public UIScreenBase screen;
+        public override int GetHashCode()
+        {
+            return screenName.GetHashCode();
+        }
     }
 }
